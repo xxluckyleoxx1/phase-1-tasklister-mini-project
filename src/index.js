68993be9de1d542d3281676c42e697bf6cad9859
Leonides <xxluckyleoxx1@gmail.com>
2021-04-09 17:28:44 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let taskForm = document.getElementById("create-task-form");
+  let taskMain = document.getElementById("main-content");
+  
+  
+  taskForm.addEventListener("submit", createNewTask)
+    
+   
+    
+    
+    
+    function createNewTask(event){
+      event.preventDefault();
+      let newTask = document.createElement("li");
+      let description = document.querySelector("#new-task-description").value
+      newTask.innerText = description;
+      let uTaskList = document.querySelector("ul#tasks");
+      uTaskList.appendChild(newTask);
+      console.log(newTask)
+    }
+})
